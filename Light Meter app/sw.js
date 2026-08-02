6d4d80ded5e6475450bfb8fc4dfcfcefad3220f8
hkun17project <hkun17project@gmail.com>
2026-08-03 01:52:01 +0800

    Delete Light Meter app directory

--- Light Meter app/sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Installed');
-});
-
-self.addEventListener('fetch', (e) => {
-  // For now, this just passes network requests through normally.
-  // We can add offline caching later.
-});
